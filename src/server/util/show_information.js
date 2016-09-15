5fe4b5b05b789dda3c62c9b0d396f931e0d7a50c
pd-smith <psmith4@buffalo.edu>
2016-09-14 21:34:48 -0400

    Move Video Server Directory Functions to own Module

---
+++ src/server/util/show_information.js
@@ -0,0 +1,50 @@
+import * as config from './config.js';
+import fs from 'fs';
+
+export function listShowDirectory(showTitle){
+    const videoPath = config.getVideoPath() + showTitle;
+  	var directoryContent = [];
+  	var files = fs.readdirSync(videoPath);
+    for(var i in files){
+  	   var stat = fs.statSync(videoPath + "/" + files[i]);
+       if(stat.isDirectory()){
+      	  directoryContent.push(files[i]);
+      	  var episodeCollection = fs.readdirSync(videoPath + "/" + files[i]);
+      	  for(var j in episodeCollection){
+      		    shows.push(episodeCollection[j]);
+      	  }
+       }
+    }
+    return directoryContent;
+}
+
+export function listShows(){
+  	var shows = [];
+  	var files = fs.readdirSync(config.getVideoPath());
+  	for(var i in files){
+  		shows.push(files[i]);
+  	}
+  	return shows;
+}
+
+export function findShow(showTitle){
+    var files = fs.readdirSync(config.getVideoPath());
+    for(var i in files){
+        if(files[i] === showTitle){
+          console.log('Show Found');
+          return true;
+        }
+    }
+    return false;
+}
+
+export function findSeason(showTitle,season){
+    var files = fs.readdirSync(config.getVideoPath() + title);
+    for(var i in files){
+        if(files[i] === season){
+          console.log("Season found");
+          return true;
+        }
+    }
+    return false;
+}
