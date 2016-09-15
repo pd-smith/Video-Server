1ce4e8f38e964bbe114e8dff41a71e1dddd2c3e6
pd-smith <psmith4@buffalo.edu>
2016-09-14 21:55:06 -0400

    Removed All old and ugly Code. Time for a fresh start

--- public/js/pageFragments.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-      $("#slideMenu").load("pages/slideMenu.html");
-});
--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import http from 'http';
-import server from 'socket.io';
-import ss from 'socket.io-stream';
 import path from 'path';
 import fs from 'fs';
 import * as config from './util/config.js';
@@ -25,84 +23,4 @@ export function main() {
 
     http.Server(app).listen(3000);
     console.log("Listening on LocalHost:3000");
-}
-
-
-
-function addSocketListeners() {
-    let io = new server();
-    io.on('connection', function(socket) {
-        ss(socket).on('upload_episode', function(stream,data){
-            //console.log("File Upload. Size: " + (data.size/1000000) + " MB");
-            if(findShow(data.name) === false){
-                fs.mkdirSync(VIDEO_PATH + data.name);
-                fs.mkdirSync(META_PATH + data.name + "/meta");
-            }
-            if(findSeason(data.name,data.season) === false){
-                fs.mkdirSync(VIDEO_PATH + data.name + "/" + data.season);
-            }
-            stream.pipe(fs.createWriteStream(VIDEO_PATH + data.name + "/" + data.season + "/" + data.episode));
-
-        });
-
-        ss(socket).on('upload_ova', function(stream,data){
-            //console.log("File Upload. Size: " + (data.size/1000000) + " MB");
-            if(findShow(data.name) === false){
-                fs.mkdirSync(VIDEO_PATH + data.name);
-                fs.mkdirSync(META_PATH + data.name);
-            }
-            if(findSeason(data.name,"OVA") === false){
-                fs.mkdirSync(VIDEO_PATH + data.name + "/OVA");
-            }
-            stream.pipe(fs.createWriteStream(VIDEO_PATH + data.name +"/OVA/" + data.episode));
-
-        });
-
-        ss(socket).on('upload_picture' , function(stream,data){
-            stream.pipe(fs.createWriteStream(META_PATH + data.name + "/show_image" + data.exten));
-        });
-
-        ss(socket).on('upload_desc' , function(stream,data){
-            stream.pipe(fs.createWriteStream(META_PATH + data.name + "/desc.txt"));
-        });
-
-        ss(socket).on('upload_movie', function(stream,data){
-            //console.log("File Upload. Size: " + (data.size/1000000) + " MB");
-            if(findShow(data.name) === false){
-                fs.mkdirSync(VIDEO_PATH + data.name);
-                fs.mkdirSync(META_PATH + data.name);
-            }
-            if(findSeason(data.name,"Movies") === false){
-                fs.mkdirSync(VIDEO_PATH + data.name + "/Movies");
-            }
-            stream.pipe(fs.createWriteStream(VIDEO_PATH + data.name+ "/Movies/" + data.title));
-
-        });
-
-        ss(socket).on("show_retrieve", function(title,uuid){
-            ss(socket).emit("show_retrieve_complete",listDir(title),uuid);
-        });
-
-        ss(socket).on("episode_retrieve", function(title,season,episode, uuid){
-            var stream = fs.createReadStream(VIDEO_PATH+title +"/"+season+"/"+episode);
-            ss(socket).emit("episode_stream",stream,uuid);
-            //stream.pipe(socket);
-
-        });
-
-        ss(socket).on("episode_info", function(title,uuid){
-            var image = getImage(title);
-            var desc = getDesc(title);
-
-            ss(socket).emit("episode_info",{img_path:image,desc:desc}, uuid);
-        });
-
-        ss(socket).on("pop_list_req", function(){
-            ss(socket).emit("pop_list", listShows());
-        });
-
-        ss(socket).on('finished', function(data){
-            console.log("Upload Complete!");
-        });
-    });
 }
